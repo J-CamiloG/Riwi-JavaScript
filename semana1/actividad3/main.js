c9dbeabc4c02f947f7b79efa98915912afd8aa7b
J-CamiloG <jc6381878@gmail.com>
2024-04-17 17:03:09 -0500

    actividad 3

---
+++ semana1/actividad3/main.js
@@ -0,0 +1,71 @@
+// creacion de numeros 
+
+// let numero1 = 45
+// let numero2 = Number(45)
+// let number3 = new Number(45)
+
+// console.log(numero1)
+// console.log(numero2)
+// console.log(number3)
+
+// como saber el limite mayor y menor de un numero en js
+
+// console.log(Number.MAX_VALUE);
+// console.log(Number.MIN_VALUE);
+
+// operaciones matematicas 
+
+// const num1 = 5;
+// const num2 = "10";
+
+// console.log(num1 + num2);  //concatenaria 
+// console.log(num1 - num2);   //daria la respuesta
+// console.log(num1 * num2);   //daria la respuesta
+// console.log(num1 / num2);   //daria la respuesta
+// console.log(num1 % num2);   //daria la respuesta
+
+// console.log(num1 ** num2);  //daria la respuesta
+
+// let num4 = 10;
+// let num5 = 10.1;
+
+// console.log(Number.isInteger(num1)); // si es entero es true
+// console.log(Number.isInteger(num2)); // si no es entero es false
+
+// let numero = 45;
+
+// console.info(numero.toString());
+// console.info(numero.toString(2));//convierte el numeor en codigo binario 
+// console.info(numero.toFixed(1));
+// console.info(numero)
+// console.info(numero.toString(16));//color exagecimal 
+
+// clase math 
+
+const numero = 45.28;
+const numero2 = 20;
+console.log();
+
+console.log(Math.round(numero));
+console.log(Math.sqrt(9999));
+console.log(Math.cbrt(numero2));
+console.log(2**20);
+console.log(Math.cos(numero2));
+console.log(Math.sin(numero2));
+console.log(Math.tan(numero2));
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.random());
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.ceil(Math.random() * 100));
+console.log(Math.trunc(Math.random() * 100));// ignora los decimales 
+console.log(Math.sign(numero2));
+console.log(Math.sign(-numero2));
+console.log(Math.max());// te dira cual es el mayo
+console.log(Math.min());
+console.log(Math.floor());
+
+var x = Math.random()*(10-20)+20;
+
+console.log(x);
