b3bd2b394fcec31f62afdebad79c7677be1d0a59
J-CamiloG <jc6381878@gmail.com>
2024-04-18 18:47:24 -0500

    ejercicios actividad 4

---
+++ semana1/actividad4/ajercicios/main.js
@@ -0,0 +1,152 @@
+console.groupCollapsed("Ejercicio 1");
+// Ejercicio 1
+// ¿Es 35 mayor que 25?
+console.log(35>25);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 2");
+// Ejercicio 2
+// ¿Es 45 menor que 30?
+console.log(45<30);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 3");
+// Ejercicio 3
+// ¿Son 50 y "50" iguales?
+console.log(50=50);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 4");
+// Ejercicio 4
+// ¿Es 40 estrictamente igual a 40 convertido a número?
+console.log(40===40);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 5");
+// Ejercicio 5
+// ¿Son "apple" y "orange" diferentes?
+console.log("apple" != "orange");
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 6");
+// Ejercicio 6
+// ¿Es "apple" estrictamente diferente de "Apple"?
+console.log("apple" === "Aple");
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 7");
+// Ejercicio 7
+// ¿Cuál es el valor de una variable sin definir?
+let number 
+console.log(number);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 8");
+// Ejercicio 8
+// ¿Cuál es el tipo de datovalor de una variable sin definir?
+let numbertypo 
+console.log(typeof(numbertypo));
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 9");
+// Ejercicio 9
+// . ¿Es null igual a undefined?
+console.log(null == undefined);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 10");
+// Ejercicio 10
+// .¿Son null y undefined estrictamente iguales?
+console.log(null === undefined);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 11");
+// Ejercicio 11
+// .¿Es "2.75" mayor que 2.5?
+console.log(2.75 > 2.5);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 12");
+// Ejercicio 12
+// ¿Es "2.75" mayor que 2.75?
+console.log(2.75 > 2.75);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 13");
+// Ejercicio 13
+// 3.¿Es "2.75" mayor o igual a 2.75?
+console.log(2.75 >= 2.75);
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 14");
+// Ejercicio 14
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 15");
+// Ejercicio 15
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 16");
+// Ejercicio 16
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 17");
+// Ejercicio 17
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 18");
+// Ejercicio 18
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 19");
+// Ejercicio 19
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 20");
+// Ejercicio 20
+let numero = 25;
+numero = numero >> 1; 
+numero = numero >> 1; 
+console.log(numero); 
+
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 21");
+// Ejercicio 21
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 22");
+// Ejercicio 22
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 23");
+// Ejercicio 23
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 24");
+// Ejercicio 24
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 25");
+// Ejercicio 25
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 26");
+// Ejercicio 26
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 27");
+// Ejercicio 27
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 28");
+// Ejercicio 28
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 29");
+// Ejercicio 29
+console.groupEnd();
+
+console.groupCollapsed("Ejercicio 30");
+// Ejercicio 30
+console.groupEnd();
---
+++ semana1/actividad4/clases/main.js
@@ -0,0 +1,36 @@
+let variables1= 156;
+let variables2= "156";
+let variables3= "hola";
+let variables4= true;
+
+console.log("variable 1:", typeof(variables1));
+console.log("variable 2:", typeof(variables2));
+console.log("variable 3:", typeof(variables3));
+console.log("variable 4:", typeof(variables4));
+
+
+let number1 = 20;
+let number2= 44;
+
+let suma = number1 + number2;
+let resta = number1 - number2;
+let multiplicacion = number1 * number2;
+let division = number1 / number2;
+let modulo = number1 % number2;
+
+console.log("suma:", suma);
+console.log("resta:", resta);
+console.log("multiplicacion:", multiplicacion);
+console.log("division:", division);
+console.log("modulo:", modulo);
+
+//operadores de incremento y decremento 
+
+let numero = 10;
+numero++
+numero--
+numero+=30
+
+//operadores logicos
+
+
