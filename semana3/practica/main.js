3a381061b18cfd153ff3922ff8449ca1ab7fefeb
J-CamiloG <jc6381878@gmail.com>
2024-04-28 20:28:55 -0500

    practica IndexOf semana 3

--- semana3/practica/main.js
+++ semana3/practica/main.js
@@ -24,6 +24,22 @@ function quitarDuplicados(numeros) {
     return duplicados
 }
 
+function  quitarDuplicadosSinOtroArray (numeros) {
+    let indice = 0 
+    while (indice < numeros.length) {
+        const elemento = numeros[indice]
+        const indiceDuplicado = numeros.indexOf(elemento, indice + 1)
+
+        if (indiceDuplicado !== -1) {
+            numeros.splice(indiceDuplicado, 1)
+        } else {
+            indice++
+        }
+    }
+}
+
+quitarDuplicadosSinOtroArray(numeros)
+console.log(numeros);
 console.log(quitarDuplicados(numeros))
 encontrarNumeroMayor(numeros)
 console.log(Math.max(...numeros));
