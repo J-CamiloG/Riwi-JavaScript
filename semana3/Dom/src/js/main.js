2a1613da6c09e3d95262d922e69656047e2efa62
J-CamiloG <jc6381878@gmail.com>
2024-04-29 12:10:53 -0500

    Dom

---
+++ semana3/Dom/src/js/main.js
@@ -0,0 +1,8 @@
+let one = document.querySelector('.btn-primary')
+let two = document.querySelector('.danger')
+let three = document.querySelector('.btn-three')
+let four = document.querySelector('.')
+
+
+
+
